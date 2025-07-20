37eca8689084f58b0155f9e4fd60576eea2f3f22
Vasyl Herman 🌥 <vasilgerman@gmail.com>
2025-07-20 12:44:31 +0300

    init

---
+++ lib/express-app/controllers/getNoteDataController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { getNoteDataService } from '../services';
+import { tryCatch } from '../utils/tryCatch';
+import { z } from 'zod';
+
+export const getNoteDataController = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const maybeNoteId = z.nanoid().safeParse(req.params.note);
+
+  if (!maybeNoteId.success) {
+    res.status(500).json({
+      msg: 'ERR',
+      data: maybeNoteId.error.issues,
+    });
+    return;
+  }
+
+  const { data, error } = await tryCatch(getNoteDataService(maybeNoteId.data));
+
+  if (error) {
+    console.error('getNoteDataService:', error);
+    res.status(500).json({
+      msg: 'ERR',
+      data: `getNoteDataService: ${error.message}`,
+    });
+    return;
+  }
+
+  const maybeNoteData = z.base64().safeParse(data);
+
+  if (!maybeNoteData.success) {
+    res.status(500).json({
+      msg: 'ERR',
+      data: maybeNoteData.error.issues,
+    });
+    return;
+  }
+
+  res.json({ msg: 'OK', data: maybeNoteData.data });
+};
